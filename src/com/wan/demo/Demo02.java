e9d6f9ab82557cf8681644bbb2515ce188ff234a
wt <wt@qq.com>
2019-08-30 11:09:52 +0800

    初始化

---
+++ src/com/wan/demo/Demo02.java
@@ -0,0 +1,19 @@
+package com.wan.demo;
+
+public class Demo02 {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("cs");
+
+	}
+
+}
